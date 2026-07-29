56cdec4c089c662ac3912e52043b538631f3cac8
EstyWalter <estywalter4@gmail.com>
2026-07-29 18:48:42 -0400

    initial commit

---
+++ SQLUtility.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        public static string ConnectionString = "";
+        public static DataTable GetDataTable(string sqlstatement)
+        {
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
