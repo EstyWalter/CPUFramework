6c4f50db1d56b9ffd08e67be69f917dc321e9a25
EstyWalter <estywalter4@gmail.com>
2026-09-14 18:26:48 -0400

    update sproc

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -100,6 +100,35 @@ namespace CPUFramework
             return DoExecuteSql(new SqlCommand(sqlstatement), true);
         }
 
+        public static void SaveDataRow(DataRow row, string sprocname)
+        {
+            SqlCommand cmd = GetSqlCommand(sprocname);
+
+            foreach (DataColumn col in row.Table.Columns){
+                string paramname = $"@{col.ColumnName}";
+                if (cmd.Parameters.Contains(paramname))
+                {
+                    cmd.Parameters[paramname].Value = row[col.ColumnName];
+                }
+            }
+            DoExecuteSql(cmd, false);
+
+            foreach (SqlParameter p in cmd.Parameters)
+            {
+                if(p.Direction == ParameterDirection.InputOutput)
+                {
+                    string colname = p.ParameterName.Substring(1);
+                    if (row.Table.Columns.Contains(colname))
+                    {
+                        row.Table.Columns[colname].ReadOnly = false;
+                            row[colname] = p.Value;
+                        row.Table.Columns[colname].ReadOnly = true;
+
+                    }
+                }
+            }
+        }
+
         public static void ExecuteSQL(SqlCommand cmd)
         {
             DoExecuteSql(cmd, false);
